02b7b7b61b64af1cd9b9ed8199a6f9a2a33b29c5
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-06-29 17:41:03 -0400

    added contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import About from './components/About';
 import Header from './components/Header';
 import Resume from './components/Resume';
+import Contact from './components/Contact';
 import Project from './components/Project';
 import Error from './components/Error';
 
@@ -15,15 +16,16 @@ function App() {
         <Router>
           {/* Link and Routes must be within the Router */}
         <nav>
-          <Link to='/'>Home</Link>  |
-          <Link to='/about'>About</Link>  |
+          <Link to='/about'>About me</Link>  |
           <Link to='/project'>Project</Link>  |
+          <Link to='/contact'>Contact</Link>  |
           <Link to='/resume'>Resume</Link>  
         </nav>
           <Routes>
             <Route path='/' element={<Header />} />
             <Route path='/about' element={<About />} />
             <Route path='/project' element={<Project />} />
+            <Route path='/contact' element={<Contact />} />
             <Route path='/resume' element={<Resume />} />
             <Route path='*' element={<Error />} />  
       {/* always put catch all error last. */}
