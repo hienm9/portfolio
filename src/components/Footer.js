b9a229e0a3e28a8a16dc11078588977d85cdb705
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-07-06 01:49:20 -0400

    update footer with social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faGithub, faLinkedin, faStackOverFlow } from '@fortawesome/free-solid-svg-icons'
-// import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
-
+import { faGithub, faLinkedin, faStackOverflow  } from '@fortawesome/free-brands-svg-icons'
 
 
 function Footer() {
     return (
         <>
         <footer className='footer'>
-       
-        <a href="https://github.com/hienm9"><FontAwesomeIcon icon={"fa-brands fa-github"} /> </a>
-        {/* <a href="https://www.linkedin.com/in/hien-mai-a765a3126/"><FontAwesomeIcon icon={faLinkedin} /></a>
-        <a href="https://stackoverflow.com/users/18013810/user18013810"><FontAwesomeIcon icon={faStackOverFlow} /></a> */}
+        <a href="https://github.com/hienm9"><FontAwesomeIcon icon={faGithub} size='2x'/></a>
+        <a href="https://www.linkedin.com/in/hien-mai-a765a3126/"><FontAwesomeIcon icon={faLinkedin} size='2x'/></a>
+        <a href="https://stackoverflow.com/users/18013810/user18013810"><FontAwesomeIcon icon={faStackOverflow} size='2x'/></a>
         </footer>
         </>
-    )
+
+    );
 }
 
 export default Footer;
