b9095e4628d3adc930ba1e6458ecd4c6199da744
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-07-09 21:48:19 -0400

    import cards style from bootstrap for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function Project() {
   const [projects] = useState([
@@ -25,6 +29,13 @@ function Project() {
       github: 'https://github.com/hienm9/RecipeSearch'
     },
     {
+      name: 'Weather Dashboard',
+      type: 'Front End App',
+      description: 'This is a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS using a third-party APIs called https://openweathermap.org/api/one-call-api to retrieve weather data for cities.',
+      href: 'https://hienm9.github.io/Challenge-06-WeatherDashboard-ServerSide-APIs/',
+      github: 'https://github.com/hienm9/Challenge-06-WeatherDashboard-ServerSide-APIs'
+    },
+    {
       name: 'Portfolio',
       type: 'React App',
       description: 'My portfolio app using React technology',
@@ -42,13 +53,35 @@ function Project() {
 
   );
   return (
-    <div>
-      {projects.map(project => (
-        <div>
-          <p>{project.description}</p>
-        </div>
-      ))}
-    </div>
+    <section className='projects'>
+      <h2>Projects</h2>
+      <Container>
+        <Row xs={1} md={2} className="g-4">
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <Col>
+              <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This is a longer card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      <div>
+        {projects.map(project => (
+          <div>
+            <p>{project.description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
