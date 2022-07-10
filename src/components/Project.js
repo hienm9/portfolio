7cac53a78f7197b752f8f1f3505734648c4d02d3
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-07-09 20:01:11 -0400

    adding Projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,21 @@ import React, { useState } from 'react';
 
 function Project() {
   const [projects] = useState([
+
+    {
+      name: 'Coding Compass',
+      type: 'Full Stack App',
+      description: 'This is a wesite that is designed to allow you to look up potential Coding bootcamps to compare them with other ones to see which will be the best for you.',
+      href: 'https://coding-compass.herokuapp.com/',
+      github: 'https://github.com/hienm9/Coding-Compass'
+    },
+    {
+      name: 'Budget Tracker',
+      type: 'Front End App',
+      description: 'An application that allows user to add expenses and deposits to their budget with or without a connection. If the user enters transactions offline the total should be updated when they are brought back online.',
+      href: 'https://hien-budget-tracker.herokuapp.com/',
+      github: 'https://github.com/hienm9/Budget-Tracker'
+    },
     {
       name: 'Recipe Search',
       type: 'Front End App',
@@ -9,14 +24,26 @@ function Project() {
       href: 'https://drspookyfox.github.io/RecipeSearch/',
       github: 'https://github.com/hienm9/RecipeSearch'
     },
-    
+    {
+      name: 'Portfolio',
+      type: 'React App',
+      description: 'My portfolio app using React technology',
+      href: 'https://hienm9.github.io/porfolio',
+      github: 'https://github.com/hienm9/porfolio'
+    },
+    {
+      name: 'Note Taker',
+      type: 'Front End App',
+      description: 'Note Taker is an application can be used to write and save notes. This application uses an Express.js back end and will save and retrieve note data from a JSON file.',
+      href: 'https://hien-notetaker.herokuapp.com/',
+      github: 'https://github.com/hienm9/Note-Taker'
+    },
   ]
-  
-    
+
   );
-  return(
+  return (
     <div>
-      {projects.map(project=>(
+      {projects.map(project => (
         <div>
           <p>{project.description}</p>
         </div>
